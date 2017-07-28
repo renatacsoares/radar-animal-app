f6b40de692ae12e51536d73a5d7f39c3425fea7d
Renata <renata.caroline@gmail.com>
2017-07-27 21:57:28 -0300

    first commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,27 @@
-angular.module('starter.controllers', [])
+angular.module('RadarAnimal.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DenunciaCtrl', ['$scope', '$stateParams', '$state',
+    function($scope, $stateParams, $state, AnimalService) {
+      $scope.animal = {};
 
-.controller('ChatsCtrl', function($scope, Chats) {
+      // metodo executado quando se clica no botao Cadastrar na pagina de cadastro de animais
+      $scope.salvaDenuncia = function() {
+        // funcao que sera chamada quando o save do Service terminar
+        /* var callback = function() {
+          // apos salvar um animal, volta para tela de lista de animais onde o animal cadastrado deve aparecer
+          $state.go('listaDeAnimais');
+        };
+        AnimalService.save($scope.animal, callback);
+        */
+
+        alert($scope.animal.tipo + "|" + $scope.animal.situacao);
+      }
+
+
+    }
+  ])
+
+.controller('ListaCtrl', function($scope, Animais) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -11,9 +30,9 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
+  $scope.animais = Animais.all();
+  $scope.remove = function(animal) {
+    Animais.remove(animal);
   };
 })
 
@@ -21,8 +40,31 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
+
+.controller('MapsCtrl', function($scope, $ionicLoading) {
+  $scope.mapCreated = function(map) {
+    $scope.map = map;
+  };
+
+  $scope.centerOnMe = function () {
+    console.log("Centering");
+    if (!$scope.map) {
+      return;
+    }
+
+    // $scope.loading = $ionicLoading.show({
+    //   content: 'Getting current location...',
+    //   showBackdrop: false
+    // });
+
+    navigator.geolocation.getCurrentPosition(function (pos) {
+      console.log('Got pos', pos);
+//      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      $scope.map.setCenter(new google.maps.LatLng(43.07493, -69.381388));
+
+      // $scope.loading.hide();
+    }, function (error) {
+      alert('Unable to get location: ' + error.message);
+    });
   };
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,47 +1,32 @@
-angular.module('starter.services', [])
+angular.module('RadarAnimal.services', [])
 
-.factory('Chats', function() {
+.factory('Animais', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var chats = [{
+  var animais = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
+    tipo: 'Gato',
+    situacao: 'Abandonado',
+    endereco: 'Rua x'
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    tipo: 'Cachorro',
+    situacao: 'Com fome',
+    endereco: 'Rua Y'
   }];
 
   return {
     all: function() {
-      return chats;
+      return animais;
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
+    remove: function(animal) {
+      animais.splice(animais.indexOf(animal), 1);
     },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    get: function(animalId) {
+      for (var i = 0; i < animais.length; i++) {
+        if (animais[i].id === parseInt(animais)) {
+          return animais[i];
         }
       }
       return null;
